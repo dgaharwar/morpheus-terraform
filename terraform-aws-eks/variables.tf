c8da4e632f069115e3f2843e8e374f9bfe94b20e
aungkyawthu <aungkyawthu139@gmail.com>
2024-10-23 20:55:49 +0700

    Update variables.tf

--- terraform-aws-eks/variables.tf
+++ terraform-aws-eks/variables.tf
@@ -71,13 +71,13 @@ variable "cluster_version" {
 variable "subnet_ids" {
   description = "List of subnet IDs for the EKS cluster"
   type        = list(string)
-  default     = ""
+
 }
 
 variable "security_group_ids" {
   description = "List of security group IDs"
   type        = list(string)
-  default     = ""
+
 }
 
 
@@ -162,7 +162,7 @@ variable "node_group_cluster_name" {
 variable "node_group_subnet_ids" {
   description = "(Required) Identifiers of EC2 Subnets to associate with the EKS Node Group. These subnets must have the following resource tag: kubernetes.io/cluster/CLUSTER_NAME (where CLUSTER_NAME is replaced with the name of the EKS Cluster)."
     type        = list(string)
-    default     = ""
+
 }
 
 /*
