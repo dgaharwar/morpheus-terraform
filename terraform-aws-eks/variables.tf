97c780779a32af44c81d2bbd55d28241003fcbae
aungkyawthu <aungkyawthu139@gmail.com>
2024-10-23 23:00:15 +0700

    Update variables.tf

--- terraform-aws-eks/variables.tf
+++ terraform-aws-eks/variables.tf
@@ -5,12 +5,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
+
+# Configure the AWS Provider
 provider "aws" {
-region     = "us-east-1"
+  region = "us-east-1"
 }
 
 variable "region" {
