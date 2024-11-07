6040de8dd1af194fbea2707663cdcff5388a79aa
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-11-07 13:23:57 +0000

    Update cluster.tf

--- terraform-aws-eks/cluster.tf
+++ terraform-aws-eks/cluster.tf
@@ -10,7 +10,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
   vpc_config {
     subnet_ids              = var.subnet_ids
-    vpc_security_group_ids  = local.security_group_ids
+    security_groups         = local.security_group_ids
   }
 
   enabled_cluster_log_types = var.cluster_enabled_cluster_log_types
