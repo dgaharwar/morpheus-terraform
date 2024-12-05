207c6b0d42834b23dfa7c34ab8f2c3e2b528e42f
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-12-05 09:16:11 +0000

    Update variables.tf

--- ec2_v0.4_with_module/modules/key-pair/variables.tf
+++ ec2_v0.4_with_module/modules/key-pair/variables.tf
@@ -1,11 +1,13 @@
 variable "use_existing_key" {
   description = "Set to true to use an existing key pair, or false to create a new one."
   type        = bool
+  default     = true
 }
 
 variable "existing_key_name" {
   description = "The name of the existing key pair to use (if 'use_existing_key' is true)."
   type        = string
+  default     = "MorpheusApp"
 }
 
 variable "public_key" {
@@ -16,7 +18,9 @@ variable "public_key" {
 variable "key_pair_name" {
   description = "The name for the new key pair (if 'use_existing_key' is false)."
   type        = string
+  default     = "MorpheusApp"
 }
+
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
